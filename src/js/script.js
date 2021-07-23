b7bb1f145bf3ba9f3e47d90ad2d27c86b1f00ee2
Alexandr <ak4raulov@yandex.ru>
2021-07-23 17:04:32 +0300

    исправил косяк с путями

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
 	$('.carousel__inner').slick({
 		speed: 1200,	
 		fade: true,
-		prevArrow: '<button type="button" class="slick-prev"><img src="../images/left_chevron.svg"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="../images/right_chevron.svg"></button>',
+		prevArrow: '<button type="button" class="slick-prev"><img src="images/left_chevron.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="images/right_chevron.svg"></button>',
 	});
 	$('.gallery__carousel').slick({
 		speed: 1200,	
 		fade: true,
-		prevArrow: '<button type="button" class="slick-prev"><img src="../images/gallery/left.svg"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="../images/gallery/right.svg"></button>',
+		prevArrow: '<button type="button" class="slick-prev"><img src="images/gallery/left.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="images/gallery/right.svg"></button>',
 	});
 });
 
