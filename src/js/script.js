0e93fcb37ce6d33f8e76f94bc62389a337249488
Alexandr <ak4raulov@yandex.ru>
2021-07-23 15:32:40 +0300

    добавил 2 блока gallery relax

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,11 @@ $(document).ready(function(){
 		prevArrow: '<button type="button" class="slick-prev"><img src="../images/left_chevron.svg"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="../images/right_chevron.svg"></button>',
 	});
+	$('.gallery__carousel').slick({
+		speed: 1200,	
+		fade: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="../images/gallery/left.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="../images/gallery/right.svg"></button>',
+	});
 });
+
