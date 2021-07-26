d2c8cf8e43ca42fb198559a47c8858ecb9479ab4
unknown <ak4raulov@yandex.ru>
2021-07-27 00:09:25 +0300

    добавил подвал и скролл

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,23 @@
-$(document).ready(function(){
-	$('.carousel__inner').slick({
-		speed: 1200,	
-		fade: true,
-		prevArrow: '<button type="button" class="slick-prev"><img src="images/left_chevron.svg"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="images/right_chevron.svg"></button>',
-	});
-	$('.gallery__carousel').slick({
-		speed: 1200,	
-		fade: true,
-		prevArrow: '<button type="button" class="slick-prev"><img src="images/gallery/left.svg"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="images/gallery/right.svg"></button>',
-	});
-});
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        fade: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/left_chevron.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/right_chevron.svg"></button>',
+    });
+    $('.gallery__carousel').slick({
+        speed: 1200,
+        fade: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/gallery/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/gallery/right.svg"></button>',
+    });
+    //прячем скролл
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1000) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
 
+});
